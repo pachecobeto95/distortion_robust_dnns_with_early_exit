e8250d4efba8e94ecb936e0d6af215692d2005b4
root <root@pechincha.gta.ufrj.br>
2021-05-10 16:26:11 -0300

    edge init

--- appEdge/__init__.py
+++ appEdge/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, g
 from appEdge.api.controllers import api     # import controlers => where thre is url to receive data
-import torch
+import torch, os, config
 import torchvision.models as models
 #from appEdge.api.services.utils import load_model
 #from appEdge.api.services.edgeProcessing import decision_maker
@@ -9,10 +9,9 @@ import torchvision.models as models
 app = Flask(__name__, static_folder="static")
 
 
-@app.before_first_request
-def before_first_request():
-	print("Execute Before Request")
-
+#@app.before_first_request
+#def before_first_request():
+#	os.system("tc qdisc add dev eth0 root netem rate 1mbit")
 
 app.config.from_object("config")
 app.config['JSON_AS_ASCII'] = False
