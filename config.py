c170a5df96fd5c370c112727cab101c1e9b9884b
root <root@pechincha.gta.ufrj.br>
2021-05-11 09:27:31 -0300

    config file

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ DEBUG = True
 DECISION_PERIOD = 30
 
 # Edge Configuration 
-HOST_EDGE = "127.0.0.1"
-PORT_EDGE = 50001
+HOST_EDGE = "172.17.0.2"
+PORT_EDGE = 5000
 URL_EDGE = "http://%s:%s"%(HOST_EDGE, PORT_EDGE)
 SAVE_IMAGES_PATH_EDGE = os.path.join(DIR_NAME, "appEdge", "images")
 SAVE_COMMUNICATION_TIME_PATH = os.path.join(DIR_NAME, "appEdge", "api", "communication_time", "communication_time_b_alexnet.csv")
@@ -18,7 +18,7 @@ PROCESSING_TIME_CLOUD_GPU_PATH = os.path.join(DIR_NAME, "appEdge", "api", "proce
 PROCESSING_TIME_CLOUD_CPU_PATH = os.path.join(DIR_NAME, "appEdge", "api", "processing_time", "processing_time_b_alexnet_cpu_cloud.csv")
 OUTPUT_FEATURE_BYTES_SIZE = os.path.join(DIR_NAME, "appEdge", "api", "output_bytes_size", "output_feature_bytes_size_alexnet.csv")
 NR_EARLY_EXITS_PATH = os.path.join(DIR_NAME, "appEdge", "api", "branchynet_data", "nr_early_exits_b_alexnet.csv")
-DISTORTION_CLASSIFIER_MODEL_PATH = os.path.join(DIR_NAME, "appEdge", "api", "models", "distortionNet.pt")
+DISTORTION_CLASSIFIER_MODEL_PATH = os.path.join(DIR_NAME, "appEdge", "api", "models", "distortionNetV2.pth")
 SAVE_DISTORTED_IMG = os.path.join(DIR_NAME, "distorted_imgs")
 
 #defining possible distortion levels
@@ -32,8 +32,8 @@ BRANCHES_POSITIONS = [2, 5, 7]
 N_BRANCHES = 3
 
 #Cloud Configuration 
-HOST_CLOUD = "127.0.0.1"
-PORT_CLOUD = 4000
+HOST_CLOUD = "146.164.69.144"
+PORT_CLOUD = 3000
 URL_CLOUD = "http://%s:%s"%(HOST_CLOUD, PORT_CLOUD)
 SAVE_IMAGES_PATH_CLOUD = os.path.join(DIR_NAME, "appCloud", "images")
 
@@ -53,11 +53,16 @@ RESULTS_INFERENCE_TIME_CLOUD = os.path.join(DIR_NAME, "appCloud", "api", "servic
 EDGE_PRISTINE_MODEL_PATH = os.path.join(".", "appEdge", "api", "services", "models", "pristine_model_b_mobilenet_caltech_17.pth")
 EDGE_BLUR_MODEL_PATH = os.path.join(".", "appEdge", "api", "services", "models", "gaussian_blur_distorted_model_mobilenet_caltech_21_freezing_3.pth")
 EDGE_NOISE_MODEL_PATH = os.path.join(".", "appEdge", "api", "services", "models", "gaussian_noise_distorted_model_mobilenet_caltech_21_freezing_3.pth")
-
+#EDGE_BLUR_MODEL_PATH = "./appEdge/api/services/models/total_gaussian_blur_distorted_model_mobilenet_caltech_21_7.pth"
 #Inference Time Result Paths of EDGE MODEL
 RESULTS_INFERENCE_TIME_EDGE = os.path.join(DIR_NAME, "appEdge", "api", "services", "result")
 
-save_idx_path = os.path.join(DIR_NAME, "dataset", "save_idx_b_mobilenet_caltech_21.npy")
-dataset_path = os.path.join(DIR_NAME, "dataset", "256_ObjectCategories", "256_ObjectCategories")
+save_idx_path = os.path.join(DIR_NAME, "save_idx_b_mobilenet_caltech_21.npy")
+dataset_path = os.path.join(DIR_NAME, "dataset", "256_ObjectCategories")
 
+#BANDWIDTH = 1 #mbps
+#LATENCY = 1 #ms
 
+#CURRENT_DISTORTION_TYPE = "pristine"
+
+P_TAR = 0.8
