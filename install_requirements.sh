6c38c98c299e766b04e8358135752e2d8e809c82
root <root@pechincha.gta.ufrj.br>
2021-05-21 14:37:56 -0300

    updating install requirements

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 apt update
 apt install python3-pip
-pip install torch==1.7.1+cu101 torchvision==0.8.2+cu101 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
-pip install opencv-python
 pip install Flask
 pip install -r requirements.txt
-
+pip install torch==1.7.1+cu101 torchvision==0.8.2+cu101 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
+pip install Flask
+pip install requests
+pip install -r requirements.txt
